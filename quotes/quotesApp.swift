89568f243d1e14da3f44672e374502b2426bfd6b
yining wang <ywang@lcs.on.ca>
2021-02-19 21:45:30 -0500

    Initial Commit

---
+++ quotes/quotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  quotesApp.swift
+//  quotes
+//
+//  Created by Yining Wang on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct quotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
